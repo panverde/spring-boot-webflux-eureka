e8d32bc99ba12fe48692d149adc5bd68d3405a16
Joaquin Alberto Huauya Arontico <jhuauyaa@everis.com>
2019-10-28 18:06:22 -0500

    first commit

---
+++ src/main/java/com/joaquin/app/SpringBootWebfluxEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.joaquin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringBootWebfluxEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxEurekaApplication.class, args);
+	}
+
+}
